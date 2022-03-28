062a89ff08f0c6eb825323227fdd89e9b45c6b14
hiro <yordansullca@gmail.com>
2022-03-28 15:15:04 -0500

    Booleanos y buenas practicas

---
+++ 06-Booleans/js/01-app.js
@@ -0,0 +1,16 @@
+const boolean1 = true;
+const boolean2 = true;
+const boolean3 = 'true';
+
+console.log(boolean1);
+console.log(boolean2);
+console.log(boolean3);
+
+console.log(typeof boolean1);
+
+// Comparador
+console.log(boolean1 == boolean3);
+console.log(boolean1 === boolean3);
+
+const boolean4 = new Boolean(true);
+console.log(boolean4);
---
+++ 06-Booleans/js/02-app.js
@@ -0,0 +1,7 @@
+const boolean1 = true;
+const boolean2 = true;
+
+console.log(boolean1 === boolean2);
+
+console.log(boolean1 === true);
+console.log(boolean1 === "true");
